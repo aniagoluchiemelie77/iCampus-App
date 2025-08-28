fac93f4a00bf01c5e6b9970de646727af0a7d94b
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2025-08-28 04:05:00 +0100

    npm final setup

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -14,8 +14,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'red',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
   },
   title: {
     fontSize: 28,
