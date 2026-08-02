a690c2da9a4b75d683e4deed30eccc9664702444
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2026-08-02 10:46:42 +0100

    Stiill on build

--- android/app/src/main/java/com/tempproject/MainApplication.kt
+++ android/app/src/main/java/com/tempproject/MainApplication.kt
@@ -27,14 +27,7 @@ class MainApplication : Application(), ReactApplication {
       }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(
-        context = applicationContext,
-        packageList = PackageList(this).packages,
-        jsMainModulePath = "index",
-        jsBundleAssetPath = "index.android.bundle",
-        isHermesEnabled = BuildConfig.IS_HERMES_ENABLED,
-        useDevSupport = BuildConfig.DEBUG
-    )
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
